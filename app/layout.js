89f50ce033c5ffb58f1ca65f3ed3585d4534e984
lanx7ufc-cloud <lanx7ufc@gmail.com>
2026-05-09 23:42:53 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'CapDrawn',
+  description: 'MemeShorts'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
